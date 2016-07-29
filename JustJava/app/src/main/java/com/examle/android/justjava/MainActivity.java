a140cc35853fa3e244d093d9cc0d8474d30ae6f0
David Rodriguez <david.rodriguez@oxus.cl>
2016-07-29 18:40:06 -0400

    Update customize name and price based on toppings

--- JustJava/app/src/main/java/com/examle/android/justjava/MainActivity.java
+++ JustJava/app/src/main/java/com/examle/android/justjava/MainActivity.java
@@ -3,6 +3,8 @@ package com.examle.android.justjava;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import java.text.NumberFormat;
@@ -18,33 +20,49 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void submitOrder(View view) {
-        int price = quantity * 5;
-        String priceMessage = "Total: $" + price + "\nThank you!";
-        displayMessage(priceMessage);
+        CheckBox whippedCreamCheck = (CheckBox) findViewById(R.id.whipped_cream_checkbox);
+        boolean statusWhippedCream = whippedCreamCheck.isChecked();
+
+        EditText nameText = (EditText) findViewById(R.id.name_edit_text);
+        String name = nameText.getText().toString();
+
+        CheckBox chocolate = (CheckBox) findViewById(R.id.chocolate_checkbox);
+        boolean statusChocolate = chocolate.isChecked();
+
+        int price = calculatePrice(statusChocolate, statusWhippedCream);
+        String summary = createOrderSummary(price, statusWhippedCream, statusChocolate, name);
+        displayMessage(summary);
     }
 
     public void increment(View view) {
         quantity++;
-        display(quantity);
+        displayQuantity(quantity);
     }
 
     public void decrement(View view) {
         quantity--;
-        display(quantity);
+        displayQuantity(quantity);
     }
 
-    private void display(int number) {
-        TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
-        quantityTextView.setText("" + number);
+    private int calculatePrice(boolean chocolate, boolean whippedCream){
+        int coffePrice = 5;
+        if ( chocolate ) coffePrice++;
+        if ( whippedCream ) coffePrice++;
+        return quantity * coffePrice;
     }
 
-    private void displayPrice(int number) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(NumberFormat.getCurrencyInstance().format(number));
+    private String createOrderSummary(int price, boolean whippedCream, boolean chocolate, String name){
+
+       return "Name: "+name+"\nAdd whipped cream? "+whippedCream+"\nQuantity: "+quantity+"\nTotal: $"+price+"\n Thank u!";
     }
+    private void displayQuantity(int number) {
+       TextView quantityTextView = (TextView) findViewById(R.id.quantity_text_view);
+       quantityTextView.setText(""+quantity);
+    }
+
 
     private void displayMessage(String message) {
-        TextView priceTextView = (TextView) findViewById(R.id.price_text_view);
-        priceTextView.setText(message);
+        TextView orderSummaryTextView = (TextView) findViewById(R.id.order_summary_text_view);
+        orderSummaryTextView.setText(message);
     }
 }
